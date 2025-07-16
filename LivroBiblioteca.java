b9192563ee57985a3dde100ca2b19e6f1d021e02
Letícia de Abreu <abreuhleticia@email.com>
2025-07-16 11:29:44 -0300

    Atualizado

--- LivroBiblioteca.java
+++ LivroBiblioteca.java
@@ -1,3 +1,16 @@
-public class LivroBiblioteca extends Livro{
-    
+public class LivroBiblioteca extends Livro{    
+    //Atributos
+    String localPrateleira;
+    String categoria;
+    int prazoEntrega;
+    String nomeQuemEmprestou;
+
+    //Metodos
+    public void Alugar(){
+        System.out.println("Estou alugando..");
+    }
+
+    public void Devolver(){
+        System.out.println("Estou devolvendo..");
+    }
 }
